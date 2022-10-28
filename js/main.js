67868462baa0bf3397cbfb72788fa349034b8691
Giovana Sanches <giovanaclemente2139@gmail.com>
2022-10-28 13:39:49 -0300

    condições js

--- js/main.js
+++ js/main.js
@@ -4,12 +4,21 @@ function tocaSom(idElementAudio) {
 const ListaDeTeclas = document.querySelectorAll(".tecla");
 
 for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
-
   const tecla = ListaDeTeclas[contador];
   const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`; //template string
 
   tecla.onclick = function () {
     tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (event) {
+    if (event.code === "Space" || event.code === "Enter") {
+      tecla.classlist.add("ativa");
+    }
+  };
+  
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
   };
 }
